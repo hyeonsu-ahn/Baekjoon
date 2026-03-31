b06411b9ae8c5ec3f893739ad1de8f24c44eda39
hyeonsu-ahn <hyeonsu.ahn@kangwon.ac.kr>
2026-03-31 18:33:12 +0900

    [Bronze IV] Title: 모음의 개수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10987. 모음의 개수/모음의 개수.py
@@ -0,0 +1,15 @@
+n = input()
+total = 0
+for x in n :
+  if 'a' in x :
+    total += 1
+  elif 'e' in x :
+    total += 1
+  elif 'i' in x :
+    total += 1
+  elif 'o' in x :
+    total += 1
+  elif 'u' in x :
+    total += 1
+
+print(total)
