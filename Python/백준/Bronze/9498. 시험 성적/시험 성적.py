80e0967f1ddc3e8224ac843942b60d1bd2d3a7c8
hyeonsu-ahn <hyeonsu.ahn@kangwon.ac.kr>
2026-04-01 16:19:18 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+li=int(input())
+
+if li >= 90 :
+  print("A")
+elif 90 > li and li >= 80 :
+  print("B")
+elif 80 > li and li >= 70 :
+  print("C")
+elif 70 > li and li >= 60 :
+  print("D")
+else :
+  print("F")
