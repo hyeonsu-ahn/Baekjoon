047027f53855d7cebcd963db8412e9be89c82615
hyeonsu-ahn <hyeonsu.ahn@kangwon.ac.kr>
2026-04-01 16:30:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main(void) {
+	long A;
+	long B;
+	long C;
+	
+
+	scanf("%ld %ld %ld", &A, &B, &C);
+	
+
+	
+	
+	
+	printf("%ld\n", A + B + C);
+
+	return 0;
+}
